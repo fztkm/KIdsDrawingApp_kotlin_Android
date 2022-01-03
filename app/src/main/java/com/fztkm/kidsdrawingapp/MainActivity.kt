fdee47065cb95d85d5a4143a50b59d6ce618d31e
Takumi Fukuzawa <forthune.0517@gmail.com>
2022-01-03 14:31:59 +0900

    ブラシサイズ選択ダイアログ

--- app/src/main/java/com/fztkm/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/fztkm/kidsdrawingapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.fztkm.kidsdrawingapp
 
+import android.app.Dialog
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
@@ -12,5 +15,40 @@ class MainActivity : AppCompatActivity() {
 
         drawingView = findViewById(R.id.drawingView)
         drawingView?.setSizeForBrush(15.toFloat())
+        val ibBrushSize: ImageButton = findViewById(R.id.ib_brush_size)
+        ibBrushSize.setOnClickListener {
+            showChoseBrushSizeDialog()
+        }
+    }
+
+    /**
+     * Show chose brush size dialog
+     * ブラシサイズ選択のダイアログを表示
+     */
+    private fun showChoseBrushSizeDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+
+        //小さいサイズ
+        val ibSmallBrush: ImageButton = brushDialog.findViewById(R.id.ib_small_brush)
+        ibSmallBrush.setOnClickListener{
+            drawingView!!.setSizeForBrush(10f)
+            brushDialog.dismiss()
+        }
+        //中サイズ
+        val ibMediumBrush: ImageButton = brushDialog.findViewById(R.id.ib_medium_brush)
+        ibMediumBrush.setOnClickListener {
+            drawingView!!.setSizeForBrush(20f)
+            brushDialog.dismiss()
+        }
+        //大きいサイズ
+        val ibLargeBrush: ImageButton = brushDialog.findViewById(R.id.ib_large_brush)
+        ibLargeBrush.setOnClickListener {
+            drawingView!!.setSizeForBrush(30f)
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
